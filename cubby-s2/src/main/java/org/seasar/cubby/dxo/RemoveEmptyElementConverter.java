ca6285801766298572629d11fcd0f72166258dcd
Yasuyuki Baba <yasuyuki.baba@gmail.com>
2006-12-25 13:45:18 +0000

    git-svn-id: https://www.seasar.org/svn/cubby/trunk/cubby@66 b6ec7606-942a-4144-8728-ba06ebfb7920

---
+++ cubby-s2/src/main/java/org/seasar/cubby/dxo/RemoveEmptyElementConverter.java
@@ -0,0 +1,11 @@
+package org.seasar.cubby.dxo;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import org.seasar.extension.dxo.annotation.DxoConverter;
+
+@Retention(RetentionPolicy.RUNTIME)
+@DxoConverter("removeEmptyValueConverter")
+public @interface RemoveEmptyElementConverter {
+}
