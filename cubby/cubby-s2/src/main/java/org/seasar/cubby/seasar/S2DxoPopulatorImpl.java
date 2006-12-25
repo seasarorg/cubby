faf6242b461747907c0cbeafaf939c8eb56eec97
Yasuyuki Baba <yasuyuki.baba@gmail.com>
2006-12-25 13:33:44 +0000

    git-svn-id: https://www.seasar.org/svn/cubby/trunk@63 b6ec7606-942a-4144-8728-ba06ebfb7920

---
+++ cubby/cubby-s2/src/main/java/org/seasar/cubby/seasar/S2DxoPopulatorImpl.java
@@ -0,0 +1,42 @@
+package org.seasar.cubby.seasar;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.seasar.cubby.convert.Populater;
+import org.seasar.cubby.dxo.HttpParameterDxo;
+import org.seasar.cubby.util.ClassUtils;
+import org.seasar.cubby.util.StringUtils;
+
+public class S2DxoPopulatorImpl implements Populater {
+
+    private HttpParameterDxo httpParameterDxo;
+
+    public void setHttpParameterDxo(final HttpParameterDxo httpParameterDxo) {
+        this.httpParameterDxo = httpParameterDxo;
+    }
+
+    public void populate(final Object target, final Map<String, Object> params) {
+        Map<String, Object> normalized = new HashMap<String, Object>();
+        for (String name : params.keySet()) {
+            Method setter = ClassUtils.getSetter(target.getClass(), name);
+            if (setter != null) {
+                Object[] values = (Object[]) params.get(name);
+                if (setter.getParameterTypes()[0].isArray()) {
+                    normalized.put(name, values);
+                } else {
+                    String value = (String) values[0];
+                    if (StringUtils.isNotEmpty(value)) {
+                        normalized.put(name, value);
+                    }
+                }
+            }
+        }
+        try {
+        	httpParameterDxo.convert(normalized, target);
+        } catch (NumberFormatException e) {
+        	// do nothing
+        }
+    }
+}
