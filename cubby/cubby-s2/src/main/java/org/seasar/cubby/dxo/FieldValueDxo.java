0bcaee86adcc0029c26dab8298e1be3432f28642
agata <agata@b6ec7606-942a-4144-8728-ba06ebfb7920>
2007-01-03 14:06:25 +0000

    git-svn-id: https://www.seasar.org/svn/cubby/trunk@86 b6ec7606-942a-4144-8728-ba06ebfb7920

--- cubby/cubby-s2/src/main/java/org/seasar/cubby/dxo/FieldValueDxo.java
+++
@@ -1,9 +0,0 @@
-package org.seasar.cubby.dxo;
-
-import org.seasar.cubby.convert.ValueConverter;
-import org.seasar.extension.dxo.annotation.DatePattern;
-
-@DatePattern("yyyy-MM-dd")
-public interface FieldValueDxo extends ValueConverter {
-	String convert(Object src);
-}
