37488ba01d960987350bee277019df182501d0e0
agata <agata@b6ec7606-942a-4144-8728-ba06ebfb7920>
2007-06-28 05:52:02 +0000

    git-svn-id: https://www.seasar.org/svn/cubby/trunk/cubby-archetype@226 b6ec7606-942a-4144-8728-ba06ebfb7920

--- target/classes/archetype-resources/src/main/java/service/impl/HelloServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package ${package}.service.impl;
-
-import ${package}.service.HelloService;
-
-public class HelloServiceImpl implements HelloService {
-	public String getMessage() {
-		return "Hello!";
-	}
-}
