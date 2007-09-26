08987cfa0ef68d3c75a03ce2116cb021724152f5
Yasuyuki Baba <yasuyuki.baba@gmail.com>
2007-09-26 19:36:47 +0000

    git-svn-id: https://www.seasar.org/svn/cubby/trunk/cubby@390 b6ec7606-942a-4144-8728-ba06ebfb7920

--- cubby/src/test/java/org/seasar/cubby/validator/impl/ActionValidatorImplTest.java
+++ cubby/src/test/java/org/seasar/cubby/validator/impl/ActionValidatorImplTest.java
@@ -4,20 +4,20 @@ import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
-import junit.framework.TestCase;
-
 import org.seasar.cubby.action.Action;
+import org.seasar.cubby.action.ActionErrors;
 import org.seasar.cubby.action.Validation;
-import org.seasar.cubby.action.impl.ActionErrorsImpl;
 import org.seasar.cubby.validator.DefaultValidationRules;
 import org.seasar.cubby.validator.PropertyValidationRule;
 import org.seasar.cubby.validator.Validator;
 import org.seasar.cubby.validator.validators.RequiredValidator;
+import org.seasar.extension.unit.S2TestCase;
 import org.seasar.framework.util.ClassUtil;
 
-public class ActionValidatorImplTest extends TestCase {
+public class ActionValidatorImplTest extends S2TestCase {
 
-	ActionValidatorImpl av = new ActionValidatorImpl();
+	ActionValidatorImpl actionValidator;
+	ActionErrors actionErrors;
 	Action action;
 	Map<String, Object> params;
 	DefaultValidationRules validators = new DefaultValidationRules();
@@ -25,8 +25,8 @@ public class ActionValidatorImplTest extends TestCase {
 	
 	@Override
 	protected void setUp() throws Exception {
-		action = new SampleAction();
-		action.setErrors(new ActionErrorsImpl());
+		include(this.getClass().getName().replaceAll("\\.", "/") + ".dicon");
+
 		params = new HashMap<String, Object>();
 		validators = new DefaultValidationRules();
 		validators.add("prop1", new RequiredValidator());
@@ -34,16 +34,16 @@ public class ActionValidatorImplTest extends TestCase {
 		Method method = ClassUtil.getMethod(SampleAction.class, "test", null);
 		validation = method.getAnnotation(Validation.class);
 	}
-	
+
 	public void testProcessValidation() {
 		params.put("prop2", "prop2 value");
-		boolean success = av.processValidation(validation, action, params, new Sample1Form(), validators);
+		boolean success = actionValidator.processValidation(validation, action, params, new Sample1Form(), validators);
 		assertFalse(success);
 	}
 
 	public void testValidateAction() {
 		params.put("prop2", "prop2 value");
-		av.validateAction(action, params, new Sample1Form(), validators);
+		actionValidator.validateAction(action, params, new Sample1Form(), validators);
 		assertEquals(1, action.getErrors().getFieldErrors().get("prop1").size());
 		assertEquals("prop1は必須です。", action.getErrors().getFieldErrors().get("prop1").get(0));
 		assertNull(action.getErrors().getFieldErrors().get("prop2"));
@@ -54,9 +54,9 @@ public class ActionValidatorImplTest extends TestCase {
 		Object form = new Object();
 		Validator req = new RequiredValidator();
 		PropertyValidationRule propValids1 = new PropertyValidationRule("prop1");
-		av.validate(action, params, form, req, propValids1);
+		actionValidator.validate(action, params, form, req, propValids1);
 		PropertyValidationRule propValids2 = new PropertyValidationRule("prop2");
-		av.validate(action, params, form, req, propValids2);
+		actionValidator.validate(action, params, form, req, propValids2);
 		assertEquals(1, action.getErrors().getFieldErrors().get("prop1").size());
 		assertEquals("prop1は必須です。", action.getErrors().getFieldErrors().get("prop1").get(0));
 		assertNull(action.getErrors().getFieldErrors().get("prop2"));
@@ -76,14 +76,14 @@ public class ActionValidatorImplTest extends TestCase {
 		Object form = new Object();
 		Validator validator = new RequiredValidator();
 		PropertyValidationRule rule1 = new PropertyValidationRule("foo.value1.bytes");
-		av.validate(action, params, form, validator, rule1);
+		actionValidator.validate(action, params, form, validator, rule1);
 		PropertyValidationRule rule2 = new PropertyValidationRule("foo.value2");
-		av.validate(action, params, form, validator, rule2);
+		actionValidator.validate(action, params, form, validator, rule2);
 		System.out.println(action.getErrors().getAllErrors());
 		assertTrue(action.getErrors().isEmpty());
 	}
 
-	class SampleAction extends Action {
+	public static class SampleAction extends Action {
 		@Validation(errorPage="error.jsp")
 		public String test() {
 			return null;
