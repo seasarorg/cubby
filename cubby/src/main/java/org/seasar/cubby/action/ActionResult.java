f90870c8e1c8910752a7637e4a9f0aea3fc67634
agata <agata@b6ec7606-942a-4144-8728-ba06ebfb7920>
2007-09-30 11:24:06 +0000

    JavaDocドキュメントやソースの整理
    git-svn-id: https://www.seasar.org/svn/cubby/trunk/cubby@431 b6ec7606-942a-4144-8728-ba06ebfb7920

--- cubby/src/main/java/org/seasar/cubby/action/ActionResult.java
+++ cubby/src/main/java/org/seasar/cubby/action/ActionResult.java
@@ -5,11 +5,27 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.seasar.cubby.controller.ActionContext;
 
+/**
+ * アクションメソッド実行後の処理を表す結果オブジェクトのインターフェイス
+ * @author agata
+ */
 public interface ActionResult {
 
+	/**
+	 * 処理を実行します。
+	 * @param context アクションコンテキスト
+	 * @param request リクエスト
+	 * @param response レスポンス
+	 * @throws Exception
+	 */
 	void execute(ActionContext context, HttpServletRequest request,
 			HttpServletResponse response) throws Exception;
 
+	/**
+	 * フォワード直前の処理を実行します。
+	 * @param context アクションコンテキスト
+	 * @param request リクエスト
+	 */
 	void prerender(ActionContext context, HttpServletRequest request);
 
 }
--- cubby/src/main/java/org/seasar/cubby/validator/ValidationRules.java
+++ cubby/src/main/java/org/seasar/cubby/validator/ValidationRules.java
@@ -2,6 +2,15 @@ package org.seasar.cubby.validator;
 
 import java.util.List;
 
+/**
+ * 入力検証の定義を保持するクラスのインターフェイスです。
+ * @author agata
+ *
+ */
 public interface ValidationRules {
+	/**
+	 * 入力検証の定義を取得します。
+	 * @return
+	 */
 	List<ValidationRule> getRules();
 }
