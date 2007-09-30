487195047a4efb5531be0b09f59488d3a81356a5
agata <agata@b6ec7606-942a-4144-8728-ba06ebfb7920>
2007-09-30 11:24:06 +0000

    JavaDocドキュメントやソースの整理
    git-svn-id: https://www.seasar.org/svn/cubby/trunk@431 b6ec7606-942a-4144-8728-ba06ebfb7920

--- cubby/cubby/src/main/java/org/seasar/cubby/action/ActionErrors.java
+++ cubby/cubby/src/main/java/org/seasar/cubby/action/ActionErrors.java
@@ -3,12 +3,52 @@ package org.seasar.cubby.action;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * アクションで発生したエラーを保持するクラス。
+ * @author agata
+ */
 public interface ActionErrors {
+	/**
+	 * エラーが存在しないかどうかを判定します。
+	 * @return エラーが存在しなければtrue
+	 */
 	boolean isEmpty();
+
+	/**
+	 * 指定されたフィールドのエラーが存在するかどうかを判定します。
+	 * @param name フィールド名
+	 * @return エラーが存在すればtrue
+	 */	
 	boolean hasFieldError(String name);
+
+	/**
+	 * アクションエラーを追加します。
+	 * @param エラーメッセージ
+	 */	
 	void addActionError(String message);
+
+	/**
+	 * フィールドエラーを追加します。
+	 * @param name フィールド名
+	 * @param message エラーメッセージ
+	 */
 	void addFieldError(String name, String message);
+	
+	/**
+	 * アクションで発生したエラーの一覧を取得します。
+	 * @return アクションで発生したエラーの一覧
+	 */	
 	List<String> getAllErrors();
+		
+	/**
+	 * アクションで発生したエラーの一覧を取得します。
+	 * @return アクションで発生したエラーの一覧
+	 */
 	List<String> getActionErrors();
+	
+	/**
+	 * フィールドで発生したエラーの一覧を取得します。
+	 * @return フィールドで発生したエラーの一覧
+	 */	
 	Map<String, List<String>> getFieldErrors();
 }
--- cubby/cubby/src/main/java/org/seasar/cubby/action/FormatPattern.java
+++ cubby/cubby/src/main/java/org/seasar/cubby/action/FormatPattern.java
@@ -2,18 +2,46 @@ package org.seasar.cubby.action;
 
 import java.text.DateFormat;
 
+/**
+ * 日付や時刻のフォーマットパターンを保持するクラスです。
+ * @author baba
+ */
 public interface FormatPattern {
 
+	/**
+	 * 日付フォーマットパターンを取得します。
+	 * @return　日付フォーマットパターン
+	 */
 	String getDatePattern();
 
+	/**
+	 * 時刻フォーマットパターンを取得します。
+	 * @return 時刻フォーマットパターン
+	 */
 	String getTimePattern();
 
+	/**
+	 * 日付時刻フォーマットパターンを取得します。
+	 * @return 日付時刻パターン
+	 */
 	String getTimestampPattern();
 
+	/**
+	 * 日付フォーマットを取得します。
+	 * @return 日付フォーマット
+	 */
 	DateFormat getDateFormat();
 
+	/**
+	 * 時刻フォーマットを取得します。
+	 * @return 時刻フォーマット
+	 */
 	DateFormat getTimeFormat();
 
+	/**
+	 * 日付時刻フォーマットを取得します。
+	 * @return 日付時刻フォーマット
+	 */
 	DateFormat getTimestampFormat();
 
 }
