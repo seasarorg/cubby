9b50bc669a097ddf87a868747407f12246b51957
agata <agata@b6ec7606-942a-4144-8728-ba06ebfb7920>
2007-10-02 09:33:11 +0000

    type=radioを認識できない問題を修正
    git-svn-id: https://www.seasar.org/svn/cubby/trunk@464 b6ec7606-942a-4144-8728-ba06ebfb7920

--- cubby/cubby/src/test/java/org/seasar/cubby/tags/InputTagTest.java
+++ cubby/cubby/src/test/java/org/seasar/cubby/tags/InputTagTest.java
@@ -97,6 +97,84 @@ public class InputTagTest extends JspTagTestCase {
 //				"<input type=\"checkbox\" value=\"value1\" name=\"stringField\"  />\n", context.getResult());
 	}
 
+	public void testDoTagRadio1() throws Exception {
+		tag.setDynamicAttribute(null, "name", "stringField");
+		tag.setDynamicAttribute(null, "value", "value1");
+		tag.setDynamicAttribute(null, "checkedValue", "value1");
+		tag.setType("radio");
+		tag.doTag();
+
+		Element element = getResultAsElementFromContext();
+		String message = "valueが指定";
+		assertTrue(message, StringUtil.isEmpty(element.getValue()));
+		assertEquals(message, 4, element.getAttributes().size());
+		assertEquals(message, "radio", element.getAttributeValue("type"));
+		assertEquals(message, "value1", element.getAttributeValue("value"));
+		assertEquals(message, "stringField", element.getAttributeValue("name"));
+		assertEquals(message, "true", element.getAttributeValue("checked"));
+//		assertEquals("valueが指定",
+//				"<input type=\"checkbox\" value=\"value1\" name=\"stringField\"  checked=\"true\"/>\n", context.getResult());
+	}
+
+	public void testDoTagRadio2() throws Exception {
+		tag.setDynamicAttribute(null, "name", "stringField");
+		tag.setDynamicAttribute(null, "value", "value1");
+		tag.setDynamicAttribute(null, "checkedValue", "value2");
+		tag.setType("radio");
+		tag.doTag();
+
+		Element element = getResultAsElementFromContext();
+		String message = "valueが指定";
+		assertTrue(message, StringUtil.isEmpty(element.getValue()));
+		assertEquals(message, 3, element.getAttributes().size());
+		assertEquals(message, "radio", element.getAttributeValue("type"));
+		assertEquals(message, "value1", element.getAttributeValue("value"));
+		assertEquals(message, "stringField", element.getAttributeValue("name"));
+//		assertEquals("valueが指定",
+//				"<input type=\"checkbox\" value=\"value1\" name=\"stringField\"  />\n", context.getResult());
+	}
+
+	public void testDoTagRadio3() throws Exception {
+		FormDto form = new FormDto();
+		form.setStringField("value1");
+		context.setAttribute("__form", form, PageContext.REQUEST_SCOPE);
+		tag.setDynamicAttribute(null, "name", "stringField");
+		tag.setDynamicAttribute(null, "value", "value1");
+		tag.setType("radio");
+		tag.doTag();
+
+		Element element = getResultAsElementFromContext();
+		String message = "valueが指定";
+		assertTrue(message, StringUtil.isEmpty(element.getValue()));
+		assertEquals(message, 4, element.getAttributes().size());
+		assertEquals(message, "radio", element.getAttributeValue("type"));
+		assertEquals(message, "value1", element.getAttributeValue("value"));
+		assertEquals(message, "stringField", element.getAttributeValue("name"));
+		assertEquals(message, "true", element.getAttributeValue("checked"));
+//		assertEquals("valueが指定",
+//				"<input type=\"checkbox\" value=\"value1\" name=\"stringField\"  checked=\"true\"/>\n", context.getResult());
+	}
+
+	public void testDoTagRadio4() throws Exception {
+		FormDto form = new FormDto();
+		form.setStringField("value2");
+		context.setAttribute("__form", form, PageContext.REQUEST_SCOPE);
+		tag.setDynamicAttribute(null, "name", "stringField");
+		tag.setDynamicAttribute(null, "value", "value1");
+		tag.setType("radio");
+		tag.doTag();
+
+		Element element = getResultAsElementFromContext();
+		String message = "valueが指定";
+		assertTrue(message, StringUtil.isEmpty(element.getValue()));
+		assertEquals(message, 3, element.getAttributes().size());
+		assertEquals(message, "radio", element.getAttributeValue("type"));
+		assertEquals(message, "value1", element.getAttributeValue("value"));
+		assertEquals(message, "stringField", element.getAttributeValue("name"));
+//		assertEquals("valueが指定",
+//				"<input type=\"checkbox\" value=\"value1\" name=\"stringField\"  />\n", context.getResult());
+	}
+
 	public void testDoTagText1() throws Exception {
 		tag.setDynamicAttribute(null, "name", "stringField");
 		tag.setDynamicAttribute(null, "value", "value1");
