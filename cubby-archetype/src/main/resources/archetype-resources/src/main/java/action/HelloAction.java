611e34bedd4f60657954d377cccf3ed230053cd4
agata <agata@b6ec7606-942a-4144-8728-ba06ebfb7920>
2007-11-14 19:31:20 +0000

    サンプルを追加
    git-svn-id: https://www.seasar.org/svn/cubby/trunk/cubby@571 b6ec7606-942a-4144-8728-ba06ebfb7920

--- cubby-archetype/src/main/resources/archetype-resources/src/main/java/action/HelloAction.java
+++ cubby-archetype/src/main/resources/archetype-resources/src/main/java/action/HelloAction.java
@@ -2,19 +2,30 @@ package ${package}.action;
 
 import org.seasar.cubby.action.Action;
 import org.seasar.cubby.action.ActionResult;
+import org.seasar.cubby.action.Form;
 import org.seasar.cubby.action.Forward;
+import org.seasar.cubby.action.Validation;
+import org.seasar.cubby.validator.DefaultValidationRules;
+import org.seasar.cubby.validator.ValidationRules;
+import org.seasar.cubby.validator.validators.RequiredValidator;
 
 import ${package}.service.HelloService;
 
 public class HelloAction extends Action {
 
+	public ValidationRules validation = new DefaultValidationRules() {
+		public void initialize() {
+			add("message", new RequiredValidator());
+		}
+	};
+
 	public HelloService helloService;
 
 	public String message;
 
+	@Validation(rulesField = "validation", errorPage = "/index.jsp")
 	public ActionResult hello() {
 		this.message = helloService.getMessage();
 		return new Forward("hello.jsp");
 	}
-
 }
