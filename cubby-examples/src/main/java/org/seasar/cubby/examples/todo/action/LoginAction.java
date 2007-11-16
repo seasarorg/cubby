2a36d8c268f0cb2d9d98db16caa55f0d810d9212
Yasuyuki Baba <yasuyuki.baba@gmail.com>
2007-11-16 20:18:10 +0000

    [CUBBY-44][CUBBY-48]
    git-svn-id: https://www.seasar.org/svn/cubby/trunk@575 b6ec7606-942a-4144-8728-ba06ebfb7920

--- cubby-examples/src/main/java/org/seasar/cubby/examples/todo/action/LoginAction.java
+++ cubby-examples/src/main/java/org/seasar/cubby/examples/todo/action/LoginAction.java
@@ -5,19 +5,23 @@ import java.util.Map;
 import javax.servlet.http.HttpSession;
 
 import org.seasar.cubby.action.Action;
+import org.seasar.cubby.action.ActionErrors;
 import org.seasar.cubby.action.ActionResult;
 import org.seasar.cubby.action.Form;
 import org.seasar.cubby.action.Forward;
 import org.seasar.cubby.action.Redirect;
 import org.seasar.cubby.action.Url;
 import org.seasar.cubby.action.Validation;
+import org.seasar.cubby.controller.CubbyConfiguration;
 import org.seasar.cubby.examples.todo.entity.User;
 import org.seasar.cubby.validator.DefaultValidationRules;
+import org.seasar.cubby.validator.ValidationRule;
 import org.seasar.cubby.validator.ValidationRules;
 import org.seasar.cubby.validator.validators.RequiredValidator;
 
 /**
  * ログイン画面
+ * 
  * @author agata
  * @author baba
  */
@@ -32,6 +36,7 @@ public class LoginAction extends Action {
 		public void initialize() {
 			add("userId", new RequiredValidator());
 			add("password", new RequiredValidator());
+			add(new UserValidationRule());
 		}
 	};
 
@@ -60,34 +65,43 @@ public class LoginAction extends Action {
 	 * ログイン処理(/todo/login/process)
 	 */
 	@Form
-	@Validation(rules="loginValidation", errorPage = "/todo/login.jsp")
+	@Validation(rules = "loginValidation", errorPage = "/todo/login.jsp")
 	public ActionResult process() {
-		User user = login(userId, password);
-		if (user != null) {
-			sessionScope.put("user", user);
-			return new Redirect("/todo/");
-		} else {
-			errors.addActionError("ユーザIDかパスワードが違います。");
-			return new Forward("/todo/login.jsp");
-		}
+		return new Redirect("/todo/");
 	}
 
-	private User login(String userId, String password) {
-		User user;
-		if ("test".equals(userId) && "test".equals(password)) {
-			user = new User();
-			user.setId(1);
-			user.setName("Cubby");
-		} else {
-			user = null;
+	private class UserValidationRule implements ValidationRule {
+
+		public void apply(Map<String, Object[]> params, Object form,
+				ActionErrors errors, CubbyConfiguration cubbyConfiguration) {
+			if (userId == null || password == null) {
+				return;
+			}
+			User user = findUser(userId, password);
+			if (user != null) {
+				sessionScope.put("user", user);
+			} else {
+				errors.add("ユーザIDかパスワードが違います。", "userId", "password");
+			}
 		}
-		return user;
+
+		private User findUser(String userId, String password) {
+			User user;
+			if ("test".equals(userId) && "test".equals(password)) {
+				user = new User();
+				user.setId(1);
+				user.setName("Cubby");
+			} else {
+				user = null;
+			}
+			return user;
+		}
 	}
 
 	@Url("/todo/logout")
 	// @InvalidateSession : s2-framework 2.4.18 から
 	public ActionResult logout() {
-		session.invalidate();	// s2-framework 2.4.17 まで
+		session.invalidate(); // s2-framework 2.4.17 まで
 		return new Redirect("/todo/login/");
 	}
 
