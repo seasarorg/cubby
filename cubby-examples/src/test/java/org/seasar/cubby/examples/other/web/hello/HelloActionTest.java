696d5e5ff75d7d66e39dbfdcef0831b68c128b78
agata <agata@b6ec7606-942a-4144-8728-ba06ebfb7920>
2008-01-06 16:18:18 +0000

    ActionContextの初期化を追加してテストが通るようにした
    git-svn-id: https://www.seasar.org/svn/cubby/trunk@621 b6ec7606-942a-4144-8728-ba06ebfb7920

--- cubby-examples/src/test/java/org/seasar/cubby/examples/other/web/hello/HelloActionTest.java
+++ cubby-examples/src/test/java/org/seasar/cubby/examples/other/web/hello/HelloActionTest.java
@@ -1,22 +1,27 @@
 package org.seasar.cubby.examples.other.web.hello;
 
 import org.seasar.cubby.action.Forward;
+import org.seasar.cubby.controller.ActionContext;
+import org.seasar.cubby.controller.impl.ActionDefImpl;
 import org.seasar.cubby.unit.CubbyTestCase;
 
 public class HelloActionTest extends CubbyTestCase {
 	
 	private HelloAction action;
+	private ActionContext actionContext;
 	
     protected void setUp() throws Exception {
         include("app.dicon");
     }
     
 	public void testIndex() throws Exception {
+		actionContext.initialize(new ActionDefImpl(getComponentDef(HelloAction.class), HelloAction.class.getMethod("index", new Class[0])));
 		Forward result = (Forward) action.index();
 		assertEquals("input.jsp", result.getPath());
 	}
 	
 	public void testMessage() throws Exception {
+		actionContext.initialize(new ActionDefImpl(getComponentDef(HelloAction.class), HelloAction.class.getMethod("message", new Class[0])));
 		action.name = "name1";
 		Forward result = (Forward) action.message();
 		assertEquals("result.jsp", result.getPath());
--- cubby-examples/src/test/java/org/seasar/cubby/examples/todo/action/LoginActionTest.java
+++ cubby-examples/src/test/java/org/seasar/cubby/examples/todo/action/LoginActionTest.java
@@ -1,17 +1,29 @@
 package org.seasar.cubby.examples.todo.action;
 
 import org.seasar.cubby.action.Forward;
+import org.seasar.cubby.controller.ActionContext;
+import org.seasar.cubby.controller.impl.ActionDefImpl;
 import org.seasar.cubby.unit.CubbyTestCase;
 
 public class LoginActionTest extends CubbyTestCase {
 
 	private LoginAction action;
+	private ActionContext actionContext;
 	
     protected void setUp() throws Exception {
         include("app.dicon");
     }
 	
 	public void testIndex() throws Exception {
+		actionContext.initialize(new ActionDefImpl(getComponentDef(LoginAction.class), LoginAction.class.getMethod("index", new Class[0])));
 		assertPathEquals(Forward.class, "/todo/login.jsp", action.index());
+	}
+
+	public void testLogin() throws Exception {
+		actionContext.initialize(new ActionDefImpl(getComponentDef(LoginAction.class), LoginAction.class.getMethod("process", new Class[0])));
+		assertPathEquals(Forward.class, "/todo/login.jsp", action.process());
+		assertEquals(2, action.getErrors().getFields().size());
+		assertTrue(action.getErrors().getFields().containsKey("userId"));
+		assertTrue(action.getErrors().getFields().containsKey("password"));
 	}
 }
