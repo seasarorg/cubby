01d3ae8a73b18084ea636dfa23e174eab9d96b0a
Yasuyuki Baba <yasuyuki.baba@gmail.com>
2008-01-10 04:16:12 +0000

    git-svn-id: https://www.seasar.org/svn/cubby/trunk/cubby@632 b6ec7606-942a-4144-8728-ba06ebfb7920

--- cubby/src/main/java/org/seasar/cubby/dxo/impl/RequestParameterConverterFactoryImpl.java
+++ cubby/src/main/java/org/seasar/cubby/dxo/impl/RequestParameterConverterFactoryImpl.java
@@ -16,7 +16,7 @@
 package org.seasar.cubby.dxo.impl;
 
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -40,13 +40,17 @@ public class RequestParameterConverterFactoryImpl extends ConverterFactoryImpl {
 	private static final Logger logger = Logger
 			.getLogger(RequestParameterConverterFactoryImpl.class);
 
-	private boolean initialized;
+	private static final Converter[] EMPTY_CONVERTER_ARRAY = new Converter[0];
+
+	private static final Map<Converter, Converter> wrapperCache = new HashMap<Converter, Converter>();
 
 	private final List<String> converterContainerPaths = new ArrayList<String>();
 
 	private final List<S2Container> containers = new ArrayList<S2Container>();
 
 	private boolean includeApplicationConverters = true;
+
+	private boolean initialized = false;
 
 	public void addConverterContainerPath(final String converterContainerPath) {
 		this.converterContainerPaths.add(converterContainerPath);
@@ -63,13 +67,14 @@ public class RequestParameterConverterFactoryImpl extends ConverterFactoryImpl {
 			return;
 		}
 
-		if (includeApplicationConverters) {
-			super.initialize();
-		}
+		super.initialize();
 
-		final Map<Class<? extends Converter>, Converter> converterMap = new LinkedHashMap<Class<? extends Converter>, Converter>();
+		final Map<Class<? extends Converter>, Converter> converterMap = new HashMap<Class<? extends Converter>, Converter>();
 		for (final String path : this.converterContainerPaths) {
 			if (ResourceUtil.isExist(path)) {
+				if (logger.isDebugEnabled()) {
+					logger.log("DCUB0011", new Object[] { path });
+				}
 				final S2Container container = S2ContainerFactory.create(path);
 				containers.add(container);
 				for (final ComponentDef componentDef : container
@@ -81,20 +86,35 @@ public class RequestParameterConverterFactoryImpl extends ConverterFactoryImpl {
 						logger.log("DCUB0010", new Object[] { converter });
 					}
 				}
-			}
-		}
-
-		final List<Converter> converters = new ArrayList<Converter>(
-				converterMap.values());
-		for (final Converter converter : this.converters) {
-			if (!converterMap.containsKey(converter.getClass())) {
-				converters.add(converter);
 				if (logger.isDebugEnabled()) {
-					logger.log("DCUB0010", new Object[] { converter });
+					logger
+							.log("DCUB0012",
+									new Object[] { container.getPath() });
 				}
 			}
 		}
-		this.converters = converters.toArray(new Converter[0]);
+
+		if (includeApplicationConverters) {
+			final List<Converter> converterList = new ArrayList<Converter>(
+					converterMap.values());
+			if (logger.isDebugEnabled()) {
+				logger.log("DCUB0013", null);
+			}
+			for (final Converter converter : converters) {
+				if (!converterMap.containsKey(converter.getClass())) {
+					converterList.add(converter);
+					if (logger.isDebugEnabled()) {
+						logger.log("DCUB0010", new Object[] { converter });
+					}
+				}
+			}
+			if (logger.isDebugEnabled()) {
+				logger.log("DCUB0014", null);
+			}
+			converters = converterList.toArray(EMPTY_CONVERTER_ARRAY);
+		} else {
+			converters = converterMap.values().toArray(EMPTY_CONVERTER_ARRAY);
+		}
 
 		DisposableUtil.add(this);
 		initialized = true;
@@ -102,6 +122,7 @@ public class RequestParameterConverterFactoryImpl extends ConverterFactoryImpl {
 
 	@Override
 	public void dispose() {
+		wrapperCache.clear();
 		for (final S2Container container : this.containers) {
 			container.destroy();
 		}
@@ -115,7 +136,14 @@ public class RequestParameterConverterFactoryImpl extends ConverterFactoryImpl {
 	public Converter getConverter(final Class sourceClass, final Class destClass) {
 		initialize();
 		final Converter converter = super.getConverter(sourceClass, destClass);
-		return new DisregardExceptionConverterWrapper(converter);
+		final Converter wrapper;
+		if (wrapperCache.containsKey(converter)) {
+			wrapper = wrapperCache.get(converter);
+		} else {
+			wrapper = new DisregardExceptionConverterWrapper(converter);
+			wrapperCache.put(converter, wrapper);
+		}
+		return wrapper;
 	}
 
 	static class DisregardExceptionConverterWrapper implements Converter {
