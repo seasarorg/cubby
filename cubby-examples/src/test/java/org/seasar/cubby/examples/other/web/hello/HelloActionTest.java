fc9a881062bfd0107f20ee5734dce977fc1fd5b6
agata <agata@b6ec7606-942a-4144-8728-ba06ebfb7920>
2008-01-13 12:35:22 +0000

    CubbyTestCaseを実装
    git-svn-id: https://www.seasar.org/svn/cubby/trunk@640 b6ec7606-942a-4144-8728-ba06ebfb7920

--- cubby-examples/src/test/java/org/seasar/cubby/examples/other/web/hello/HelloActionTest.java
+++ cubby-examples/src/test/java/org/seasar/cubby/examples/other/web/hello/HelloActionTest.java
@@ -1,31 +1,25 @@
 package org.seasar.cubby.examples.other.web.hello;
 
+import org.seasar.cubby.action.ActionResult;
 import org.seasar.cubby.action.Forward;
-import org.seasar.cubby.controller.ActionContext;
-import org.seasar.cubby.controller.impl.ActionDefImpl;
 import org.seasar.cubby.unit.CubbyTestCase;
 
 public class HelloActionTest extends CubbyTestCase {
-	
 	private HelloAction action;
-	private ActionContext actionContext;
 	
     protected void setUp() throws Exception {
         include("app.dicon");
     }
     
 	public void testIndex() throws Exception {
-		actionContext.initialize(new ActionDefImpl(getComponentDef(HelloAction.class), HelloAction.class.getMethod("index", new Class[0])));
-		Forward result = (Forward) action.index();
-		assertEquals("input.jsp", result.getPath());
+		ActionResult result = processAction("/hello/");
+		assertPathEquals(Forward.class, "input.jsp", result);
 	}
 	
 	public void testMessage() throws Exception {
 		getRequest().addParameter("name", "name1");
-		actionContext.initialize(new ActionDefImpl(getComponentDef(HelloAction.class), HelloAction.class.getMethod("message", new Class[0])));
-		Forward result = (Forward) action.message();
-		assertEquals("result.jsp", result.getPath());
+		ActionResult result = processAction("/hello/message");
+		assertPathEquals(Forward.class, "result.jsp", result);
 		assertEquals("name1", action.name);
-	}
-	
+	}	
 }
--- cubby-examples/src/test/java/org/seasar/cubby/examples/todo/action/LoginActionTest.java
+++ cubby-examples/src/test/java/org/seasar/cubby/examples/todo/action/LoginActionTest.java
@@ -2,37 +2,31 @@ package org.seasar.cubby.examples.todo.action;
 
 import org.seasar.cubby.action.Forward;
 import org.seasar.cubby.action.Redirect;
-import org.seasar.cubby.controller.ActionContext;
-import org.seasar.cubby.controller.impl.ActionDefImpl;
 import org.seasar.cubby.unit.CubbyTestCase;
 
 public class LoginActionTest extends CubbyTestCase {
 
 	private LoginAction action;
-	private ActionContext actionContext;
 	
     protected void setUp() throws Exception {
         include("app.dicon");
     }
 	
 	public void testIndex() throws Exception {
-		actionContext.initialize(new ActionDefImpl(getComponentDef(LoginAction.class), LoginAction.class.getMethod("index", new Class[0])));
-		assertPathEquals(Forward.class, "/todo/login.jsp", action.index());
+		assertPathEquals(Forward.class, "/todo/login.jsp", processAction("/todo/login/"));
 	}
 
 	public void testLogin() throws Exception {
 		getRequest().addParameter("userId", "test");
 		getRequest().addParameter("password", "test");
-		actionContext.initialize(new ActionDefImpl(getComponentDef(LoginAction.class), LoginAction.class.getMethod("process", new Class[0])));
-		assertPathEquals(Redirect.class, "/todo/", action.process());
+		assertPathEquals(Redirect.class, "/todo/", processAction("/todo/login/process"));
 		assertNotNull(action.sessionScope.get("user"));
 	}
 
 	public void testLogin_authError() throws Exception {
-		actionContext.initialize(new ActionDefImpl(getComponentDef(LoginAction.class), LoginAction.class.getMethod("process", new Class[0])));
 		getRequest().addParameter("userId", "test");
 		getRequest().addParameter("password", "dummy");
-		assertPathEquals(Forward.class, "/todo/login.jsp", action.process());
+		assertPathEquals(Forward.class, "/todo/login.jsp", processAction("/todo/login/process"));
 		assertEquals(0, action.getErrors().getOthers().size());
 		assertEquals(2, action.getErrors().getFields().size());
 		assertEquals(1, action.getErrors().getFields().get("userId").size());
@@ -43,8 +37,7 @@ public class LoginActionTest extends CubbyTestCase {
 	}
 
 	public void testLogin_validationError() throws Exception {
-		actionContext.initialize(new ActionDefImpl(getComponentDef(LoginAction.class), LoginAction.class.getMethod("process", new Class[0])));
-		assertPathEquals(Forward.class, "/todo/login.jsp", action.process());
+		assertPathEquals(Forward.class, "/todo/login.jsp", processAction("/todo/login/process"));
 		assertEquals(0, action.getErrors().getOthers().size());
 		assertEquals(2, action.getErrors().getFields().size());
 		assertEquals(1, action.getErrors().getFields().get("userId").size());
