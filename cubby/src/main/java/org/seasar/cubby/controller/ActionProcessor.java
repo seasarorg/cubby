81b0d0caa9ad4efe81eaa0e84a3df01d1e98619a
agata <agata@b6ec7606-942a-4144-8728-ba06ebfb7920>
2008-01-13 12:35:26 +0000

    CubbyTestCaseを実装
    git-svn-id: https://www.seasar.org/svn/cubby/trunk/cubby@641 b6ec7606-942a-4144-8728-ba06ebfb7920

--- cubby/src/main/java/org/seasar/cubby/controller/ActionProcessor.java
+++ cubby/src/main/java/org/seasar/cubby/controller/ActionProcessor.java
@@ -4,9 +4,29 @@ import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public interface ActionProcessor {
+import org.seasar.cubby.action.ActionResult;
 
-	void process(HttpServletRequest request, HttpServletResponse response,
+/**
+ * リクエストのパスを元にアクションメソッドを決定して実行するクラスです。
+ * 
+ * @author agata
+ */
+public interface ActionProcessor {
+	
+	/**
+	 * リクエストのパスを元にアクションメソッドを決定して実行します。
+	 * <ul>
+	 * <li>リクエストパスを元に実行するアクションとそのアクションメソッドを決定します。</li>
+	 * <li>アクションメソッドを実行します。</li>
+	 * <li>アクションメソッドの実行結果である{@link ActionResult}を実行します。</li>
+	 * </ul>
+	 * @param request リクエスト
+	 * @param response レスポンス
+	 * @param chain フィルターチェイン
+	 * @return 実行結果。アクションメソッドが存在しない場合はnull。
+	 * @throws Exception
+	 */
+	ActionResult process(HttpServletRequest request, HttpServletResponse response,
 			FilterChain chain) throws Exception;
 
 }
---
+++ cubby/src/main/java/org/seasar/cubby/unit/CubbyTestCase.java
@@ -0,0 +1,111 @@
+package org.seasar.cubby.unit;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.seasar.cubby.action.ActionResult;
+import org.seasar.cubby.action.Forward;
+import org.seasar.cubby.action.Redirect;
+import org.seasar.cubby.controller.ActionProcessor;
+import org.seasar.cubby.filter.RequestRoutingFilter;
+import org.seasar.framework.unit.S2TigerTestCase;
+import org.seasar.framework.util.FieldUtil;
+
+/**
+ * CubbyのActionクラスの単体テスト用のクラスです。
+ * <p>
+ * このクラスを継承して、それぞれのActionクラス用の単体テストを作成します。
+ * 親クラスに
+ * <pre>
+ * public class HelloActionTest extends CubbyTestCase<HelloAction> {
+ *   private HelloAction action;
+ *   protected void setUp() throws Exception {
+ *     include("app.dicon");
+ *   }
+ *   public void testIndex() throws Exception {
+ *     ActionResult result = processAction("/hello/");
+ *     assertPathEquals(Forward.class, "input.jsp", result);
+ *   }
+ *   public void testMessage() throws Exception {
+ *     getRequest().addParameter("name", "name1");
+ *     ActionResult result = processAction("/hello/message");
+ *     assertPathEquals(Forward.class, "result.jsp", result);
+ *     assertEquals("name1", action.name);
+ *   }	
+ * }
+ * </pre>
+ * </p>
+ * @author agata
+ */
+public class CubbyTestCase extends S2TigerTestCase {
+	
+	/** ルーティング */
+	private RequestRoutingFilter.Router router = new RequestRoutingFilter.Router();
+
+	private MockFilterChain filterChain = new MockFilterChain();
+	
+	/** ActionProcessor */
+	private ActionProcessor actionProcessor;
+	
+	/**
+	 * ActionResultの型とパスをチェックします。
+	 * @param <T>
+	 * @param resultClass ActionResultの型
+	 * @param expectedPath 期待されるパス
+	 * @param actualResult チェックするActionResult
+	 */
+	public static void assertPathEquals(Class<? extends ActionResult> resultClass, String expectedPath,
+			ActionResult actualResult) {
+		assertEquals("ActionResultの型をチェック", resultClass, actualResult.getClass());
+		if (actualResult instanceof Forward) {
+			assertEquals("パスのチェック", expectedPath, ((Forward)actualResult).getPath());
+		} else if (actualResult instanceof Redirect) {
+			assertEquals("パスのチェック", expectedPath, ((Redirect)actualResult).getPath());
+		}
+	}
+	
+	/**
+	 * アクションメソッドを実行します。
+	 * @param string 
+	 * @param string 
+	 * @return アクションメソッドの実行結果。アクションメソッドが見つからなかったり結果がない場合、null
+	 * @throws Exception 
+	 */
+	@SuppressWarnings({ "hiding", "unchecked" })
+	protected ActionResult processAction(String orginalPath) throws Exception {
+		routing(orginalPath);
+		return actionProcessor.process(getRequest(), getResponse(), filterChain);
+	}
+	
+	/**
+	 * CubbyFilterで行っているルーティングをエミュレートして、内部フォワードパスをリクエストにセットします。
+	 * @param orginalPath オリジナルパス
+	 * @return 内部フォワードパス
+	 * @throws NoSuchFieldException
+	 */
+	protected String routing(String orginalPath) throws NoSuchFieldException {
+		Field field = getRequest().getClass().getDeclaredField("servletPath");
+		field.setAccessible(true);
+		FieldUtil.set(field, getRequest(), orginalPath);
+		String forwardPath = router.routing(getRequest(), getResponse());
+		FieldUtil.set(field, getRequest(), forwardPath);
+		return forwardPath;
+	}
+
+	/**
+	 * モックのFilterChain
+	 * @author agata
+	 */
+	private static class MockFilterChain implements FilterChain {
+		public void doFilter(ServletRequest request,
+				ServletResponse response) throws IOException,
+				ServletException {
+		}
+	}
+
+}
