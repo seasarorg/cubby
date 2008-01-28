c165df1411aa7e3e8c53b0395a234aab9885a0fb
Yasuyuki Baba <yasuyuki.baba@gmail.com>
2008-01-28 17:12:11 +0000

    [CUBBY-84] @Pathで指定したURIの優先順位を明確にする、の変更前にテスト追加
    git-svn-id: https://www.seasar.org/svn/cubby/branches/1.0.x@692 b6ec7606-942a-4144-8728-ba06ebfb7920

---
+++ cubby/cubby/src/test/java/org/seasar/cubby/convention/impl/MockAction.java
@@ -0,0 +1,27 @@
+package org.seasar.cubby.convention.impl;
+
+import org.seasar.cubby.action.ActionResult;
+import org.seasar.cubby.action.Path;
+
+public class MockAction {
+
+	public ActionResult update() {
+		return null;
+	}
+
+	@Path("create")
+	public ActionResult insert() {
+		return null;
+	}
+
+	@Path("delete/{value,[0-9]+}")
+	public ActionResult delete() {
+		return null;
+	}
+
+//	@Path("{name}")
+//	public ActionResult name() {
+//		return null;
+//	}
+
+}
