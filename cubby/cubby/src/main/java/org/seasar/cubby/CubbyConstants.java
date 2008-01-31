e9e0574940d03cc1eaaf2702eb2ed48ef13afecc
Yasuyuki Baba <yasuyuki.baba@gmail.com>
2008-01-31 17:16:19 +0000

    [CUBBY-86] 内部フォワードのパスを整理する
    git-svn-id: https://www.seasar.org/svn/cubby/branches/1.0.x@701 b6ec7606-942a-4144-8728-ba06ebfb7920

--- cubby/cubby/src/main/java/org/seasar/cubby/CubbyConstants.java
+++ cubby/cubby/src/main/java/org/seasar/cubby/CubbyConstants.java
@@ -17,57 +17,55 @@ package org.seasar.cubby;
 
 /**
  * Cubbyフレームワークで使用する定数クラスです。
+ * 
  * @author agata
+ * @author baba
  */
 public class CubbyConstants {
-	
+
 	/**
-	 * Cubbyのデフォルトメッセージリソースの名前
+	 * 内部フォワード先のディレクトリ。
+	 */
+	public static final String INTERNAL_FORWARD_DIRECTORY = "__internal_forward";
+
+	/**
+	 * Cubbyのデフォルトメッセージリソースの名前。
 	 */
 	public static final String RES_MESSAGES = "messages";
-	
+
 	/**
-	 * パラメータオブジェクトのリクエストの属性中の名前
+	 * パラメータオブジェクトのリクエストの属性中の名前。
 	 */
 	public static final String ATTR_PARAMS = "params";
 
 	/**
-	 * アクションオブジェクトのリクエストの属性中の名前
+	 * アクションオブジェクトのリクエストの属性中の名前。
 	 */
 	public static final String ATTR_ACTION = "action";
 
 	/**
-	 * バリデーションエラーを表すオブジェクトのリクエストの属性中の名前
+	 * バリデーションエラーを表すオブジェクトのリクエストの属性中の名前。
 	 */
 	public static final String ATTR_VALIDATION_FAIL = "validationFail";
 
 	/**
-	 * TODO
+	 * フォームの値を復元する際にリクエスト属性に一時的に作成する値の名前。
 	 */
 	public static final String ATTR_OUTPUT_VALUES = "outputValues";
 
 	/**
-	 * メッセージリソースをMapに変換したオブジェクトのリクエストの属性中の名前
+	 * メッセージリソースをMapに変換したオブジェクトのリクエストの属性中の名前。
 	 */
 	public static final String ATTR_MESSAGES = "messages";
 
 	/**
-	 * コンテキストパスのリクエストの属性中の名前
+	 * コンテキストパスのリクエストの属性中の名前。
 	 */
 	public static final String ATTR_CONTEXT_PATH = "contextPath";
 
 	/**
-	 * TODO
-	 */
-	public static final String ATTR_ACTION_CLASS_NAME = "org.seasar.cubby.actionClassName";
-
-	/**
-	 * TODO
-	 */
-	public static final String ATTR_METHOD_NAME = "org.seasar.cubby.methodName";
-
-	/**
-	 * TODO
+	 * トークンのMapをセッションに保存する名前
 	 */
 	public static final String ATTR_TOKEN = "org.seasar.cubby.token";
+
 }
