f76c4e986f7b77b98a8daf1ad2ce180c527352bb
Yasuyuki Baba <yasuyuki.baba@gmail.com>
2008-02-07 13:17:43 +0000

    javadoc
    git-svn-id: https://www.seasar.org/svn/cubby/branches/1.0.x@739 b6ec7606-942a-4144-8728-ba06ebfb7920

--- cubby/cubby/src/main/java/org/seasar/cubby/exception/DuplicateRoutingRuntimeException.java
+++ cubby/cubby/src/main/java/org/seasar/cubby/exception/DuplicateRoutingRuntimeException.java
@@ -18,24 +18,49 @@ package org.seasar.cubby.exception;
 import org.seasar.framework.exception.SRuntimeException;
 
 /**
- * アクションの登録時に、そのクラス名、メソッド名、アノテーションから算出したパスの正規表現が重複したことを示すためにスローされます。
+ * アクションの登録時に、そのクラス名、メソッド名、アノテーションから算出したパスの正規表現が重複したことを示すためにスローされる実行時例外です。
  * 
  * @author baba
  */
 public class DuplicateRoutingRuntimeException extends SRuntimeException {
 
+	/** シリアルバージョンUID。 */
 	private static final long serialVersionUID = 7634436382080744052L;
 
+	/**
+	 * 指定されたメッセージコードとその置換文字列および原因を使用して新規例外を構築します。
+	 * 
+	 * @param messageCode
+	 *            メッセージコード
+	 * @param args
+	 *            メッセージコードに対応するメッセージに対する置換文字列の配列
+	 * @param cause
+	 *            この例外の原因
+	 */
 	public DuplicateRoutingRuntimeException(final String messageCode,
 			final Object[] args, final Throwable cause) {
 		super(messageCode, args, cause);
 	}
 
+	/**
+	 * 指定されたメッセージコードとその置換文字列を使用して新規例外を構築します。
+	 * 
+	 * @param messageCode
+	 *            メッセージコード
+	 * @param args
+	 *            メッセージコードに対応するメッセージに対する置換文字列の配列
+	 */
 	public DuplicateRoutingRuntimeException(final String messageCode,
 			final Object[] args) {
 		super(messageCode, args);
 	}
 
+	/**
+	 * 指定されたメッセージコードを使用して新規例外を構築します。
+	 * 
+	 * @param messageCode
+	 *            メッセージコード
+	 */
 	public DuplicateRoutingRuntimeException(final String messageCode) {
 		super(messageCode);
 	}
