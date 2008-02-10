a2f44bd533d0e61fec5193b156647aea68c83d7e
Yasuyuki Baba <yasuyuki.baba@gmail.com>
2008-02-10 18:19:25 +0000

    javadoc
    git-svn-id: https://www.seasar.org/svn/cubby/branches/1.0.x@758 b6ec7606-942a-4144-8728-ba06ebfb7920

--- cubby/cubby/src/main/java/org/seasar/cubby/util/CubbyUtils.java
+++ cubby/cubby/src/main/java/org/seasar/cubby/util/CubbyUtils.java
@@ -308,4 +308,57 @@ public class CubbyUtils {
 		return tokens;
 	}
 
+	/**
+	 * 指定された文字列をHTMLとしてエスケープします。
+	 * <p>
+	 * <table> <thead>
+	 * <tr>
+	 * <th>変換前</th>
+	 * <th>変換後</th>
+	 * </tr>
+	 * </thead> <tbody>
+	 * <tr>
+	 * <td>&amp;</td>
+	 * <td>&amp;amp;</td>
+	 * </tr>
+	 * <tr>
+	 * <td>&lt;</td>
+	 * <td>&amp;lt;</td>
+	 * </tr>
+	 * <tr>
+	 * <td>&gt;</td>
+	 * <td>&amp;gt;</td>
+	 * </tr>
+	 * <tr>
+	 * <td>&quot;</td>
+	 * <td>&amp;quot;</td>
+	 * </tr>
+	 * <tr>
+	 * <td>&#39</td>
+	 * <td>&amp;#39</td>
+	 * </tr>
+	 * </tbody> </table>
+	 * </p>
+	 * 
+	 * @param str
+	 * @return エスケープされた文字列
+	 */
+	public static String escapeHtml(final Object str) {
+		if (str == null) {
+			return "";
+		}
+		String text;
+		if (str instanceof String) {
+			text = (String) str;
+		} else {
+			text = str.toString();
+		}
+		text = StringUtil.replace(text, "&", "&amp;");
+		text = StringUtil.replace(text, "<", "&lt;");
+		text = StringUtil.replace(text, ">", "&gt;");
+		text = StringUtil.replace(text, "\"", "&quot;");
+		text = StringUtil.replace(text, "'", "&#39;");
+		return text;
+	}
+
 }
