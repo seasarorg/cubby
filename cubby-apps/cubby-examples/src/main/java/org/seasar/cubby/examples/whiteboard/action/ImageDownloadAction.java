4952088032a775a0def2f6f85f20faac5d5a50b3
sakamoto <sakamoto@b6ec7606-942a-4144-8728-ba06ebfb7920>
2008-02-18 08:33:50 +0000

    サンプル、テスト動作用のcubby-appsを新規作成
    git-svn-id: https://www.seasar.org/svn/cubby/branches/1.0.x@775 b6ec7606-942a-4144-8728-ba06ebfb7920

---
+++ cubby-apps/cubby-examples/src/main/java/org/seasar/cubby/examples/whiteboard/action/ImageDownloadAction.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2004-2008 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.cubby.examples.whiteboard.action;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.seasar.cubby.action.Action;
+import org.seasar.cubby.action.ActionResult;
+import org.seasar.cubby.action.Direct;
+import org.seasar.cubby.action.Path;
+import org.seasar.cubby.examples.whiteboard.dao.PageDao;
+import org.seasar.cubby.examples.whiteboard.entity.Page;
+
+@Path("whiteboard")
+public class ImageDownloadAction extends Action {
+
+	public HttpServletResponse response;
+
+	public PageDao pageDao;
+
+	public Integer id;
+
+	@Path("api/image/{id,[0-9]+}")
+	public ActionResult upload() throws IOException {
+		Page page = pageDao.selectById(id);
+
+		response.setContentType("image/png");
+		response.setDateHeader("Last-Modified", new Date().getTime());
+
+		OutputStream output = response.getOutputStream();
+		output.write(page.getImage());
+
+		return new Direct();
+	}
+
+}
