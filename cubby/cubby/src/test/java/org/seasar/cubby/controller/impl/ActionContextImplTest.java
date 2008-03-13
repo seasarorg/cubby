1cdc88d481717fc1603adbac1361bd10bbe62389
Yasuyuki Baba <yasuyuki.baba@gmail.com>
2008-03-13 18:55:03 +0000

    [CUBBY-108][CUBBY-109][CUBBY-110][CUBBY-111]をブランチからマージ
    git-svn-id: https://www.seasar.org/svn/cubby/trunk@872 b6ec7606-942a-4144-8728-ba06ebfb7920

--- cubby/cubby/src/test/java/org/seasar/cubby/controller/impl/ActionContextImplTest.java
+++ cubby/cubby/src/test/java/org/seasar/cubby/controller/impl/ActionContextImplTest.java
@@ -19,107 +19,167 @@ import static org.seasar.cubby.TestUtils.getPrivateField;
 
 import java.util.Map;
 
-import junit.framework.TestCase;
-
+import org.seasar.cubby.controller.ActionContext;
+import org.seasar.cubby.controller.ActionDef;
+import org.seasar.cubby.controller.ActionDefBuilder;
 import org.seasar.cubby.dxo.FormDxo;
+import org.seasar.cubby.exception.ActionRuntimeException;
+import org.seasar.cubby.util.CubbyUtils;
+import org.seasar.extension.unit.S2TestCase;
+import org.seasar.framework.mock.servlet.MockHttpServletRequest;
+import org.seasar.framework.mock.servlet.MockServletContext;
 
-public class ActionContextImplTest extends TestCase {
+public class ActionContextImplTest extends S2TestCase {
 
-    public void testConstructor() throws Throwable {
-        ActionContextImpl actionContextImpl = new ActionContextImpl();
-        assertFalse("actionContextImpl.isInitialized()", actionContextImpl.isInitialized());
-    }
-    
-    public void testInitialize() throws Throwable {
-        ActionContextImpl actionContextImpl = new ActionContextImpl();
-        actionContextImpl.initialize(null);
-        assertFalse("actionContextImpl.isInitialized()", actionContextImpl.isInitialized());
-    }
-    
-    public void testIsInitialized() throws Throwable {
-        boolean result = new ActionContextImpl().isInitialized();
-        assertFalse("result", result);
-    }
-    
-    public void testSetParameterBinder() throws Throwable {
-        FormDxo formDxo = new FormDxo() {
+	public ActionContext actionContext;
+
+	public ActionDefBuilder actionDefBuilder;
+
+	public MockAction mockAction;
+
+	@Override
+	protected void setUp() throws Exception {
+		include(this.getClass().getName().replaceAll("\\.", "/") + ".dicon");
+	}
+
+	public void testConstructor() throws Throwable {
+		assertFalse("actionContextImpl.isInitialized()", actionContext
+				.isInitialized());
+	}
+
+	public void testInitialize() throws Throwable {
+		actionContext.initialize(null);
+		assertFalse("actionContextImpl.isInitialized()", actionContext
+				.isInitialized());
+	}
+
+	public void testIsInitialized() throws Throwable {
+		assertFalse("result", actionContext.isInitialized());
+	}
+
+	public void testSetParameterBinder() throws Throwable {
+		FormDxo formDxo = new FormDxo() {
 			public void convert(Map<String, Object[]> src, Object dest) {
 			}
+
 			public void convert(Object src, Map<String, String[]> dest) {
 			}
-        };
-        ActionContextImpl actionContextImpl = new ActionContextImpl();
-        actionContextImpl.setFormDxo(formDxo);
-        assertSame("actionContextImpl.getParameterBinder()", formDxo, actionContextImpl.getFormDxo());
-    }
-    
-    public void testGetActionThrowsNullPointerException() throws Throwable {
-        ActionContextImpl actionContextImpl = new ActionContextImpl();
-        try {
-            actionContextImpl.getAction();
-            fail("Expected NullPointerException to be thrown");
-        } catch (NullPointerException ex) {
-            assertNull("ex.getMessage()", ex.getMessage());
-            assertNull("actionContextImpl.action", getPrivateField(actionContextImpl, "action"));
-            assertFalse("actionContextImpl.isInitialized()", actionContextImpl.isInitialized());
-        }
-    }
-    
-    public void testGetComponentDefThrowsNullPointerException() throws Throwable {
-        ActionContextImpl actionContextImpl = new ActionContextImpl();
-        try {
-            actionContextImpl.getComponentDef();
-            fail("Expected NullPointerException to be thrown");
-        } catch (NullPointerException ex) {
-            assertNull("ex.getMessage()", ex.getMessage());
-            assertFalse("actionContextImpl.isInitialized()", actionContextImpl.isInitialized());
-        }
-    }
-    
-    public void testGetFormBeanThrowsNullPointerException() throws Throwable {
-        ActionContextImpl actionContextImpl = new ActionContextImpl();
-        try {
-            actionContextImpl.getFormBean();
-            fail("Expected NullPointerException to be thrown");
-        } catch (NullPointerException ex) {
-            assertNull("ex.getMessage()", ex.getMessage());
-            assertFalse("actionContextImpl.isInitialized()", actionContextImpl.isInitialized());
-            assertNull("actionContextImpl.action", getPrivateField(actionContextImpl, "action"));
-        }
-    }
-    
-    public void testGetMethodThrowsNullPointerException() throws Throwable {
-        ActionContextImpl actionContextImpl = new ActionContextImpl();
-        try {
-            actionContextImpl.getMethod();
-            fail("Expected NullPointerException to be thrown");
-        } catch (NullPointerException ex) {
-            assertNull("ex.getMessage()", ex.getMessage());
-            assertFalse("actionContextImpl.isInitialized()", actionContextImpl.isInitialized());
-        }
-    }
-    
-    public void testGetValidationThrowsNullPointerException() throws Throwable {
-        ActionContextImpl actionContextImpl = new ActionContextImpl();
-        try {
-            actionContextImpl.getValidation();
-            fail("Expected NullPointerException to be thrown");
-        } catch (NullPointerException ex) {
-            assertNull("ex.getMessage()", ex.getMessage());
-            assertFalse("actionContextImpl.isInitialized()", actionContextImpl.isInitialized());
-        }
-    }
-    
-    public void testInvokeThrowsNullPointerException() throws Throwable {
-        ActionContextImpl actionContextImpl = new ActionContextImpl();
-        try {
-            actionContextImpl.invoke();
-            fail("Expected NullPointerException to be thrown");
-        } catch (NullPointerException ex) {
-            assertNull("ex.getMessage()", ex.getMessage());
-            assertFalse("actionContextImpl.isInitialized()", actionContextImpl.isInitialized());
-            assertNull("actionContextImpl.action", getPrivateField(actionContextImpl, "action"));
-        }
-    }
-}
+		};
+		ActionContextImpl actionContextImpl = new ActionContextImpl();
+		actionContextImpl.setFormDxo(formDxo);
+		assertSame("actionContextImpl.getParameterBinder()", formDxo,
+				actionContextImpl.getFormDxo());
+	}
 
+	public void testGetActionThrowsNullPointerException() throws Throwable {
+		try {
+			actionContext.getAction();
+			fail("Expected NullPointerException to be thrown");
+		} catch (NullPointerException ex) {
+			assertNull("ex.getMessage()", ex.getMessage());
+			assertNull("actionContextImpl.action", getPrivateField(
+					actionContext, "action"));
+			assertFalse("actionContextImpl.isInitialized()", actionContext
+					.isInitialized());
+		}
+	}
+
+	public void testGetComponentDefThrowsNullPointerException()
+			throws Throwable {
+		try {
+			actionContext.getComponentDef();
+			fail("Expected NullPointerException to be thrown");
+		} catch (NullPointerException ex) {
+			assertNull("ex.getMessage()", ex.getMessage());
+			assertFalse("actionContextImpl.isInitialized()", actionContext
+					.isInitialized());
+		}
+	}
+
+	public void testGetFormBeanThrowsNullPointerException() throws Throwable {
+		try {
+			actionContext.getFormBean();
+			fail("Expected NullPointerException to be thrown");
+		} catch (NullPointerException ex) {
+			assertNull("ex.getMessage()", ex.getMessage());
+			assertFalse("actionContextImpl.isInitialized()", actionContext
+					.isInitialized());
+			assertNull("actionContextImpl.action", getPrivateField(
+					actionContext, "action"));
+		}
+	}
+
+	public void testGetMethodThrowsNullPointerException() throws Throwable {
+		try {
+			actionContext.getMethod();
+			fail("Expected NullPointerException to be thrown");
+		} catch (NullPointerException ex) {
+			assertNull("ex.getMessage()", ex.getMessage());
+			assertFalse("actionContextImpl.isInitialized()", actionContext
+					.isInitialized());
+		}
+	}
+
+	public void testGetValidationThrowsNullPointerException() throws Throwable {
+		try {
+			actionContext.getValidation();
+			fail("Expected NullPointerException to be thrown");
+		} catch (NullPointerException ex) {
+			assertNull("ex.getMessage()", ex.getMessage());
+			assertFalse("actionContextImpl.isInitialized()", actionContext
+					.isInitialized());
+		}
+	}
+
+	public void testInvokeThrowsNullPointerException() throws Throwable {
+		try {
+			actionContext.invoke();
+			fail("Expected NullPointerException to be thrown");
+		} catch (NullPointerException ex) {
+			assertNull("ex.getMessage()", ex.getMessage());
+			assertFalse("actionContextImpl.isInitialized()", actionContext
+					.isInitialized());
+			assertNull("actionContextImpl.action", getPrivateField(
+					actionContext, "action"));
+		}
+	}
+
+	public void testGetFormObject1() throws Exception {
+		MockServletContext servletContext = getServletContext();
+		MockHttpServletRequest request = servletContext
+				.createRequest(CubbyUtils.getInternalForwardPath(
+						MockAction.class, "update"));
+		ActionDef actionDef = actionDefBuilder.build(request);
+		actionContext.initialize(actionDef);
+		Object form = actionContext.getFormBean();
+		assertSame(mockAction, form);
+	}
+
+	public void testGetFormObject2() throws Exception {
+		MockServletContext servletContext = getServletContext();
+		MockHttpServletRequest request = servletContext
+				.createRequest(CubbyUtils.getInternalForwardPath(
+						MockAction.class, "legalForm"));
+		ActionDef actionDef = actionDefBuilder.build(request);
+		actionContext.initialize(actionDef);
+		Object form = actionContext.getFormBean();
+		assertSame(mockAction.form, form);
+	}
+
+	public void testGetFormObject3() throws Exception {
+		MockServletContext servletContext = getServletContext();
+		MockHttpServletRequest request = servletContext
+				.createRequest(CubbyUtils.getInternalForwardPath(
+						MockAction.class, "illegalForm"));
+		ActionDef actionDef = actionDefBuilder.build(request);
+		actionContext.initialize(actionDef);
+		try {
+			actionContext.getFormBean();
+			fail();
+		} catch (ActionRuntimeException e) {
+			// ok
+			e.printStackTrace();
+		}
+	}
+
+}
