7de163049d83c93e43f3291dac0143b453350ea6
Yasuyuki Baba <yasuyuki.baba@gmail.com>
2008-03-19 19:19:46 +0000

    [CUBBY-93]バリデーションのタイミング指定
    git-svn-id: https://www.seasar.org/svn/cubby/trunk/cubby-apps@883 b6ec7606-942a-4144-8728-ba06ebfb7920

--- cubby-examples/src/main/java/org/seasar/cubby/examples/todo/entity/User.java
+++ cubby-examples/src/main/java/org/seasar/cubby/examples/todo/entity/User.java
@@ -17,20 +17,33 @@ package org.seasar.cubby.examples.todo.entity;
 
 import java.io.Serializable;
 
+import org.seasar.dao.annotation.tiger.Id;
+
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 4405198629442011216L;
 
-	private Integer id;
+	private String id;
+
+	private String password;
 
 	private String name;
 
-	public Integer getId() {
+	@Id
+	public String getId() {
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(String id) {
 		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 	public String getName() {
