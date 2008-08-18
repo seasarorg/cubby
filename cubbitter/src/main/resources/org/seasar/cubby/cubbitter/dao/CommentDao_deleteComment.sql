64a42e039130034c747f7d8af6d5316de2139f6f
Yasuyuki Baba <yasuyuki.baba@gmail.com>
2008-08-18 11:55:13 +0000

    いったんコミット
    git-svn-id: https://www.seasar.org/svn/cubby/trunk/cubby-apps@1159 b6ec7606-942a-4144-8728-ba06ebfb7920

--- cubbitter/src/main/resources/org/seasar/cubby/cubbitter/dao/CommentDao_deleteComment.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM favorite_comments WHERE comment_id = /*commentId*/1 ;
-DELETE FROM member_comments WHERE comment_id = /*commentId*/1 ;
-DELETE FROM reply_comments WHERE comment_id = /*commentId*/1 ;
-DELETE FROM comment WHERE comment_id = /*commentId*/1 ;
--- cubbitter/src/main/resources/org/seasar/cubby/cubbitter/dao/CommentDao_insertComment.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO comment(comment, post_time) values(/*comment*/'aaa' , now());
-INSERT INTO member_comments(member_id, comment_id) SELECT /*memberId*/5, currval('comment_comment_id_seq');
-/*IF replyMemberId > 0*/
-INSERT INTO reply_comments(member_id, comment_id) SELECT /*replyMemberId*/5, currval('comment_comment_id_seq');
-/*END*/
