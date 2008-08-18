65de920598434bb867ee8af7c8eddb51e84a241b
Yasuyuki Baba <yasuyuki.baba@gmail.com>
2008-08-18 11:55:13 +0000

    いったんコミット
    git-svn-id: https://www.seasar.org/svn/cubby/trunk@1159 b6ec7606-942a-4144-8728-ba06ebfb7920

--- cubby-apps/cubbitter/src/main/resources/org/seasar/cubby/cubbitter/dao/CommentDao_deleteCommentList.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM favorite_comments WHERE member_id = /*memberId*/1 ;
-DELETE FROM member_comments WHERE member_id = /*memberId*/1 ;
-DELETE FROM reply_comments WHERE member_id = /*memberId*/1 ;
-/*IF !commentIdList.isEmpty()*/DELETE FROM comment WHERE comment_id IN /*commentIdList*/(100, 200) ;/*END*/
--- cubby-apps/cubbitter/src/main/resources/org/seasar/cubby/cubbitter/dao/ReplyCommentsDao_getReplyComments.sql
+++
@@ -1,8 +0,0 @@
-SELECT c.*, (fc.member_id IS NOT NULL) as favorite
-FROM reply_comments rc
-INNER JOIN comment_view c
-ON c.comment_id = rc.comment_id
-LEFT OUTER JOIN favorite_comments fc
-ON fc.member_id = /*memberId*/1
-AND fc.comment_id = rc.comment_id
-WHERE rc.member_id = /*memberId*/1
