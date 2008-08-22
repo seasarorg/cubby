56bdf64500c618ad7184f91f1d1c66f0d8912da2
Yasuyuki Baba <yasuyuki.baba@gmail.com>
2008-08-22 18:26:13 +0000

    git-svn-id: https://www.seasar.org/svn/cubby/trunk@1176 b6ec7606-942a-4144-8728-ba06ebfb7920

--- cubby-apps/cubbitter/src/main/java/org/seasar/cubby/cubbitter/dao/ImageDao.java
+++
@@ -1,8 +0,0 @@
-package org.seasar.cubby.cubbitter.dao;
-
-import org.seasar.cubby.cubbitter.entity.Image;
-import org.seasar.kuina.dao.GenericDao;
-
-public interface ImageDao extends GenericDao<Image, Long>{
-
-}
