817b8d9611ee4ad70c7c332404ba5ac59dfebb31
Yasuyuki Baba <yasuyuki.baba@gmail.com>
2008-09-02 17:21:57 +0000

    [CUBBY-181] ConverterのサンプルをExamplesに追加する
    git-svn-id: https://www.seasar.org/svn/cubby/trunk/cubby-apps@1228 b6ec7606-942a-4144-8728-ba06ebfb7920

---
+++ cubby-examples/src/main/java/org/seasar/cubby/examples/other/web/converter/ConverterAction.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2004-2008 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.cubby.examples.other.web.converter;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.seasar.cubby.action.Action;
+import org.seasar.cubby.action.ActionErrors;
+import org.seasar.cubby.action.ActionResult;
+import org.seasar.cubby.action.Forward;
+import org.seasar.cubby.action.Path;
+import org.seasar.cubby.action.RequestParameter;
+import org.seasar.cubby.action.SendError;
+import org.seasar.cubby.action.Validation;
+import org.seasar.cubby.validator.ValidationException;
+import org.seasar.cubby.validator.ValidationRule;
+import org.seasar.cubby.validator.ValidationRules;
+
+public class ConverterAction extends Action {
+
+	public BookDao bookDao;
+
+	public List<Book> books;
+
+	@RequestParameter
+	public Book book;
+
+	@Override
+	public void prerender() {
+		this.books = bookDao.findAll();
+	}
+
+	public ActionResult index() {
+		return new Forward("index.jsp");
+	}
+
+	public ValidationRules detailValidationRules = new AbstractValidationRules() {
+
+		@Override
+		protected void initialize() {
+			add(RESOURCE, new ValidationRule() {
+
+				public void apply(Map<String, Object[]> params, Object form,
+						ActionErrors errors) throws ValidationException {
+					// バリデーション実行前に
+					// ・コンバータによる String から Book への変換
+					// ・アクションのプロパティへのバインド
+					// の実行は終わっているので、
+					// この時点でプロパティを検査して null の場合は
+					// クライアントへ 404 NOT FOUND を返す。
+					ConverterAction action = ConverterAction.class.cast(form);
+					if (action.book == null) {
+						throw new ValidationException(new SendError(
+								HttpServletResponse.SC_NOT_FOUND));
+					}
+				}
+
+			});
+		}
+
+	};
+
+	@Path("{book,...-..........}")
+	@Validation(rules = "detailValidationRules", errorPage = "index.jsp")
+	public ActionResult detail() {
+		return new Forward("index.jsp");
+	}
+
+}
