427ad68347d3ba744ff3204181c6f5ffec10c8a9
agata <agata@b6ec7606-942a-4144-8728-ba06ebfb7920>
2008-09-20 17:24:17 +0000

    r1259-r1262をマージ(1.1.xから)
    git-svn-id: https://www.seasar.org/svn/cubby/trunk/cubby-project@1263 b6ec7606-942a-4144-8728-ba06ebfb7920

--- cubby/src/test/java/org/seasar/cubby/tags/MockFormTagTestAction.java
+++ cubby/src/test/java/org/seasar/cubby/tags/MockFormTagTestAction.java
@@ -1,9 +1,10 @@
 package org.seasar.cubby.tags;
 
+import org.seasar.cubby.action.Action;
 import org.seasar.cubby.action.ActionResult;
 import org.seasar.cubby.action.Path;
 
-public class MockFormTagTestAction {
+public class MockFormTagTestAction extends Action {
 
 	public ActionResult foo() {
 		return null;
