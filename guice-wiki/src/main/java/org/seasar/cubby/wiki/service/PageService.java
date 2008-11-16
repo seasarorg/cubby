1d90245afe213db7bfe9c9bf2d38ceeed49b2acb
agata <agata@b6ec7606-942a-4144-8728-ba06ebfb7920>
2008-11-16 01:34:05 +0000

    とりあえずコミット
    git-svn-id: https://www.seasar.org/svn/cubby/branches/2.0.x/cubby-apps@1319 b6ec7606-942a-4144-8728-ba06ebfb7920

--- guice-wiki/src/main/java/org/seasar/cubby/wiki/service/PageService.java
+++ guice-wiki/src/main/java/org/seasar/cubby/wiki/service/PageService.java
@@ -1,32 +1,35 @@
 package org.seasar.cubby.wiki.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.seasar.cubby.wiki.entity.Page;
-import org.seasar.extension.jdbc.JdbcManager;
 
 
 public class PageService {
 
-	public JdbcManager jdbcManager;
+//	public JdbcManager jdbcManager;
 	
 	public Page getPageById(Integer id) {
-		return jdbcManager.from(Page.class).id(id).getSingleResult();
+//		return jdbcManager.from(Page.class).id(id).getSingleResult();
+		return new Page();
 	}
 
 	public Page getPageByName(String name) {
-		return jdbcManager.from(Page.class).where("name=?", name).getSingleResult();
+//		return jdbcManager.from(Page.class).where("name=?", name).getSingleResult();
+		return new Page();
 	}
 
 	public void save(Page page) {
-		if (page.getId() == null) {
-			jdbcManager.insert(page).excludesNull().execute();
-		} else {
-			jdbcManager.update(page).excludesNull().execute();
-		}
+//		if (page.getId() == null) {
+//			jdbcManager.insert(page).excludesNull().execute();
+//		} else {
+//			jdbcManager.update(page).excludesNull().execute();
+//		}
 	}
 
 	public List<Page> getPages() {
-		return jdbcManager.from(Page.class).orderBy("name").getResultList();
+//		return jdbcManager.from(Page.class).orderBy("name").getResultList();
+		return new ArrayList<Page>();
 	}
 }
