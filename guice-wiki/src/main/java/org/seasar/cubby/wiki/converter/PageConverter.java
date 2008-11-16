53126ac31e15891dad5429038b7691bb6b9e7ff0
agata <agata@b6ec7606-942a-4144-8728-ba06ebfb7920>
2008-11-16 15:17:19 +0000

    だいたい動作するようになりました
    git-svn-id: https://www.seasar.org/svn/cubby/branches/2.0.x/cubby-apps@1328 b6ec7606-942a-4144-8728-ba06ebfb7920

--- guice-wiki/src/main/java/org/seasar/cubby/wiki/converter/PageConverter.java
+++ guice-wiki/src/main/java/org/seasar/cubby/wiki/converter/PageConverter.java
@@ -5,15 +5,19 @@ import org.seasar.cubby.converter.impl.AbstractConverter;
 import org.seasar.cubby.wiki.entity.Page;
 import org.seasar.cubby.wiki.service.PageService;
 
+import com.google.inject.Inject;
+
 
 public class PageConverter extends AbstractConverter {
 
-	public PageService pageService;
+	@Inject
+	private PageService pageService;
 	
 	public Object convertToObject(Object value, Class<?> objectType,
 			ConversionHelper helper) {
 		String name = (String) value;
-		return pageService.getPageByName(name);
+		Page page = pageService.getPageByName(name);
+		return page;
 	}
 
 	public String convertToString(Object value, ConversionHelper helper) {
---
+++ guice-wiki/src/main/java/org/seasar/cubby/wiki/filter/TransactionFilter.java
@@ -0,0 +1,49 @@
+package org.seasar.cubby.wiki.filter;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.seasar.cubby.plugins.guice.ModuleFactory;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+public class TransactionFilter implements Filter {
+	Injector injector;
+	public void init(FilterConfig config) throws ServletException {
+		injector = Guice.createInjector(ModuleFactory.getModule());
+	}
+
+	public void destroy() {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		EntityManager entityManager = null;
+		EntityTransaction transaction = null;
+		boolean commit = false;
+		try {
+			entityManager = injector.getInstance(EntityManager.class);
+			transaction = entityManager.getTransaction();
+			transaction.begin();
+			chain.doFilter(request, response);
+			transaction.commit();
+			commit = true;
+		} finally {
+			if (transaction != null && transaction.isActive() && !commit) {
+				transaction.rollback();
+			}
+			if (entityManager != null) {
+				entityManager.close();
+			}
+		}
+	}
+}
