4c9723527037ac0183a1816db3a31c8bd426ad97
Yasuyuki Baba <yasuyuki.baba@gmail.com>
2008-12-14 10:29:15 +0000

    CubbyRunner に Filter を指定できるように変更、guice-servlet への依存を排除
    git-svn-id: https://www.seasar.org/svn/cubby/branches/2.0.x/cubby-project@1363 b6ec7606-942a-4144-8728-ba06ebfb7920

--- cubby/src/main/java/org/seasar/cubby/unit/CubbyRunner.java
+++ cubby/src/main/java/org/seasar/cubby/unit/CubbyRunner.java
@@ -2,11 +2,19 @@ package org.seasar.cubby.unit;
 
 import static org.seasar.cubby.CubbyConstants.ATTR_PARAMS;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -30,28 +38,72 @@ public class CubbyRunner {
 	 *             アクションメソッドの実行時に例外が発生した場合
 	 */
 	public static ActionResult processAction(final HttpServletRequest request,
-			final HttpServletResponse response) throws Exception {
+			final HttpServletResponse response, final Filter... filters)
+			throws Exception {
+		final ActionInvokeFilterChain chain = new ActionInvokeFilterChain(
+				filters);
+		chain.doFilter(request, response);
+		return chain.getActionResult();
+	}
 
-		MockPathProccessorImpl pathProccessor = new MockPathProccessorImpl(
-				request, response, new ArrayList<Pattern>());
-		final ActionResultWrapper actionResultWrapper = pathProccessor
-				.doProcess();
-		if (actionResultWrapper == null) {
-			return null;
+	private static class ActionInvoker {
+		public ActionResult invoke(final HttpServletRequest request,
+				final HttpServletResponse response) throws Exception {
+			final MockPathProccessorImpl pathProccessor = new MockPathProccessorImpl(
+					request, response, new ArrayList<Pattern>());
+			final ActionResultWrapper actionResultWrapper = pathProccessor
+					.doProcess();
+			if (actionResultWrapper == null) {
+				return null;
+			}
+			return actionResultWrapper.getActionResult();
 		}
-		return actionResultWrapper.getActionResult();
+	}
+
+	private static class ActionInvokeFilterChain implements FilterChain {
+
+		private final Iterator<Filter> iterator;
+
+		private ActionResult actionResult = null;
+
+		public ActionInvokeFilterChain(final Filter... filters) {
+			this.iterator = Arrays.asList(filters).iterator();
+		}
+
+		public void doFilter(final ServletRequest request,
+				final ServletResponse response) throws IOException,
+				ServletException {
+			if (iterator.hasNext()) {
+				final Filter filter = iterator.next();
+				filter.doFilter(request, response, this);
+			} else {
+				try {
+					this.actionResult = new ActionInvoker().invoke(
+							(HttpServletRequest) request,
+							(HttpServletResponse) response);
+				} catch (final Exception e) {
+					throw new ServletException(e);
+				}
+			}
+		}
+
+		public ActionResult getActionResult() {
+			return actionResult;
+		}
+
 	}
 
 	protected static class MockPathProccessorImpl extends PathProcessorImpl {
 
-		private HttpServletRequest request;
+		private final HttpServletRequest request;
 
-		private HttpServletResponse response;
+		private final HttpServletResponse response;
 
-		private ActionProcessor actionProcessor = new ActionProcessorImpl();
+		private final ActionProcessor actionProcessor = new ActionProcessorImpl();
 
-		public MockPathProccessorImpl(HttpServletRequest request,
-				HttpServletResponse response, List<Pattern> ignorePathPatterns) {
+		public MockPathProccessorImpl(final HttpServletRequest request,
+				final HttpServletResponse response,
+				final List<Pattern> ignorePathPatterns) {
 			super(request, response, ignorePathPatterns);
 			this.request = request;
 			this.response = response;
