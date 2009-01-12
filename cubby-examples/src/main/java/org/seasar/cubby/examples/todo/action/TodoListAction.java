8721cd175bf57d15b6c8ed4f844f56b4f49cc240
Yasuyuki Baba <yasuyuki.baba@gmail.com>
2009-01-12 18:43:36 +0000

    設定を変更
    git-svn-id: https://www.seasar.org/svn/cubby/branches/2.0.x/cubby-apps@1416 b6ec7606-942a-4144-8728-ba06ebfb7920

--- cubby-examples/src/main/java/org/seasar/cubby/examples/todo/action/TodoListAction.java
+++ cubby-examples/src/main/java/org/seasar/cubby/examples/todo/action/TodoListAction.java
@@ -20,10 +20,10 @@ import java.util.List;
 
 import org.seasar.cubby.action.ActionResult;
 import org.seasar.cubby.action.Form;
-import org.seasar.cubby.action.FormatPattern;
 import org.seasar.cubby.action.Forward;
 import org.seasar.cubby.action.Path;
 import org.seasar.cubby.action.Validation;
+import org.seasar.cubby.controller.FormatPattern;
 import org.seasar.cubby.examples.todo.dao.TodoDao;
 import org.seasar.cubby.examples.todo.dao.TodoTypeDao;
 import org.seasar.cubby.examples.todo.dto.TodoConditionDto;
