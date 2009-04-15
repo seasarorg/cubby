d34a29344ef518588c9680d58343a2b188fee4d4
Yasuyuki Baba <yasuyuki.baba@gmail.com>
2009-04-15 17:23:59 +0000

    S2JSONSerializerPlugin のサンプル
    git-svn-id: https://www.seasar.org/svn/cubby/branches/2.0.x/cubby-project@1503 b6ec7606-942a-4144-8728-ba06ebfb7920

--- cubby-s2/src/test/java/org/seasar/cubby/plugins/s2/S2JSONSerializerPlugin.java
+++ cubby-s2/src/test/java/org/seasar/cubby/plugins/s2/S2JSONSerializerPlugin.java
@@ -5,23 +5,27 @@ import org.seasar.cubby.spi.JsonProvider;
 import org.seasar.cubby.spi.Provider;
 import org.seasar.framework.util.JSONSerializer;
 
-public class S2JsonPlugin extends AbstractPlugin {
+/**
+ * {@link JSONSerializer} にって {@link JsonProvider} を提供するプラグインのサンプルです。
+ * 
+ * @author baba
+ */
+@SuppressWarnings("deprecation")
+public class S2JSONSerializerPlugin extends AbstractPlugin {
 
-	private JsonProvider jsonProvider = new S2JsonProvider();
+	private JsonProvider jsonProvider = new JsonProvider() {
+
+		public String toJson(Object o) {
+			return JSONSerializer.serialize(o);
+		}
+
+	};
 
 	public <S extends Provider> S getProvider(Class<S> service) {
 		if (JsonProvider.class.equals(service)) {
 			return service.cast(jsonProvider);
 		}
 		return null;
-	}
-
-	static class S2JsonProvider implements JsonProvider {
-
-		public String toJson(Object o) {
-			return JSONSerializer.serialize(o);
-		}
-
 	}
 
 }
