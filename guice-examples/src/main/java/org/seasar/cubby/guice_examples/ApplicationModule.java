8226c9865d4bb939a817187d5377b84239f1e2a5
Yasuyuki Baba <yasuyuki.baba@gmail.com>
2009-04-21 13:33:49 +0000

    RequestParser の設定を簡素化と guice の archetype 改善
    git-svn-id: https://www.seasar.org/svn/cubby/branches/2.0.x/cubby-apps@1533 b6ec7606-942a-4144-8728-ba06ebfb7920

--- guice-examples/src/main/java/org/seasar/cubby/guice_examples/ApplicationModule.java
+++ guice-examples/src/main/java/org/seasar/cubby/guice_examples/ApplicationModule.java
@@ -1,5 +1,13 @@
 package org.seasar.cubby.guice_examples;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUpload;
+import org.apache.commons.fileupload.RequestContext;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.fileupload.servlet.ServletRequestContext;
 import org.seasar.cubby.guice_examples.action.HelloAction;
 import org.seasar.cubby.guice_examples.action.IndexAction;
 import org.seasar.cubby.guice_examples.service.HelloService;
@@ -11,7 +19,11 @@ import org.seasar.cubby.routing.impl.PathResolverImpl;
 import org.seasar.cubby.routing.impl.PathTemplateParserImpl;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
 import com.google.inject.Singleton;
+import com.google.inject.servlet.RequestScoped;
+import com.google.inject.servlet.ServletModule;
 
 public class ApplicationModule extends AbstractModule {
 
@@ -22,15 +34,67 @@ public class ApplicationModule extends AbstractModule {
 			@Override
 			protected PathResolver getPathResolver() {
 				final PathTemplateParser pathTemplateParser = new PathTemplateParserImpl();
-				final PathResolver pathResolver = new PathResolverImpl(pathTemplateParser);
+				final PathResolver pathResolver = new PathResolverImpl(
+						pathTemplateParser);
 				pathResolver.add(IndexAction.class);
 				pathResolver.add(HelloAction.class);
 				return pathResolver;
 			}
 
 		});
+		install(new ServletModule());
 
 		bind(HelloService.class).to(HelloServiceImpl.class).in(Singleton.class);
+
+		configureFileUpload();
+	}
+
+	/**
+	 * commons-fileupload の設定を行います。
+	 * <p>
+	 * {@link org.apache.commons.fileupload.disk.DiskFileItemFactory}
+	 * はテンポラリにローカルファイルシステムを使用します。Goolgle&nbsp;App&nbsp;Engine
+	 * のようにファイルシステムへの書き込みに制限がある場合は
+	 * {@link org.apache.commons.fileupload.disk.DiskFileItemFactory} ではなく、
+	 * {@link org.seasar.cubby.fileupload.StreamFileItemFactory} を試してみてください。
+	 * これはマルチパートのデータをテンポラリを使用せずにオンメモリで処理します。
+	 * </p>
+	 */
+	protected void configureFileUpload() {
+		final FileItemFactory fileItemFactory = new DiskFileItemFactory();
+		bind(FileUpload.class).toInstance(
+				new ServletFileUpload(fileItemFactory));
+		bind(RequestContext.class).toProvider(RequestContextProvider.class).in(
+				RequestScoped.class);
+	}
+
+	/**
+	 * {@link RequestContext} のプロバイダです。
+	 */
+	private static class RequestContextProvider implements
+			Provider<RequestContext> {
+
+		/** {@link RequestContext} です。 */
+		private RequestContext requestContext;
+
+		/**
+		 * インスタンス化します。
+		 * 
+		 * @param request
+		 *            要求
+		 */
+		@Inject
+		public RequestContextProvider(final HttpServletRequest request) {
+			this.requestContext = new ServletRequestContext(request);
+		}
+
+		/**
+		 * {@link RequestContext} を取得します。
+		 */
+		public RequestContext get() {
+			return requestContext;
+		}
+
 	}
 
 }
