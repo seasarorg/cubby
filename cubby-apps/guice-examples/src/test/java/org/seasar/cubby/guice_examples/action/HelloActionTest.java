4288b622a4738352eac0ef7eef3ba59e01686f9d
Yasuyuki Baba <yasuyuki.baba@gmail.com>
2009-04-25 20:40:09 +0000

    CubbyRunner を使ったテストが通らなくなっていたので修正
    git-svn-id: https://www.seasar.org/svn/cubby/branches/2.0.x@1549 b6ec7606-942a-4144-8728-ba06ebfb7920

--- cubby-apps/guice-examples/src/test/java/org/seasar/cubby/guice_examples/action/HelloActionTest.java
+++ cubby-apps/guice-examples/src/test/java/org/seasar/cubby/guice_examples/action/HelloActionTest.java
@@ -1,72 +1,64 @@
 package org.seasar.cubby.guice_examples.action;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.seasar.cubby.action.ActionResult;
 import org.seasar.cubby.action.Forward;
 import org.seasar.cubby.guice_examples.ApplicationModule;
-import org.seasar.cubby.plugin.PluginRegistry;
 import org.seasar.cubby.plugins.guice.GuicePlugin;
 import org.seasar.cubby.unit.CubbyAssert;
 import org.seasar.cubby.unit.CubbyRunner;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockServletContext;
 
-import com.google.inject.Guice;
 import com.google.inject.servlet.GuiceFilter;
 
 public class HelloActionTest {
 
-	private PluginRegistry pluginRegistry = PluginRegistry.getInstance();
-
-	@Before
-	public void before() {
-		GuicePlugin guicePlugin = new GuicePlugin();
-		guicePlugin.setInjector(Guice.createInjector(new ApplicationModule()));
-		pluginRegistry.register(guicePlugin);
-	}
-
-	@After
-	public void after() {
-		pluginRegistry.clear();
-	}
-
 	@Test
 	public void index1() throws Exception {
+		MockServletContext servletContext = new MockServletContext();
+		servletContext.addInitParameter(GuicePlugin.MODULE_INIT_PARAM_NAME,
+				ApplicationModule.class.getName());
 		MockHttpServletRequest request = new MockHttpServletRequest();
 		request.setMethod("GET");
 		request.setServletPath("/hello/");
 		MockHttpServletResponse response = new MockHttpServletResponse();
 
-		ActionResult actualResult = CubbyRunner.processAction(request,
-				response, new GuiceFilter());
+		ActionResult actualResult = CubbyRunner.processAction(servletContext,
+				request, response, new GuiceFilter());
 		CubbyAssert.assertPathEquals(Forward.class, "index.jsp", actualResult);
 	}
 
 	@Test
 	public void message1() throws Exception {
+		MockServletContext servletContext = new MockServletContext();
+		servletContext.addInitParameter(GuicePlugin.MODULE_INIT_PARAM_NAME,
+				ApplicationModule.class.getName());
 		MockHttpServletRequest request = new MockHttpServletRequest();
 		request.setMethod("GET");
 		request.setServletPath("/hello/message");
 		MockHttpServletResponse response = new MockHttpServletResponse();
 
-		ActionResult actualResult = CubbyRunner.processAction(request,
-				response, new GuiceFilter());
+		ActionResult actualResult = CubbyRunner.processAction(servletContext,
+				request, response, new GuiceFilter());
 		CubbyAssert.assertPathEquals(Forward.class, "index.jsp", actualResult);
 
 	}
 
 	@Test
 	public void message2() throws Exception {
+		MockServletContext servletContext = new MockServletContext();
+		servletContext.addInitParameter(GuicePlugin.MODULE_INIT_PARAM_NAME,
+				ApplicationModule.class.getName());
 		MockHttpServletRequest request = new MockHttpServletRequest();
 		request.setMethod("GET");
 		request.setServletPath("/hello/message");
 		request.addParameter("name", "cubby");
 		MockHttpServletResponse response = new MockHttpServletResponse();
 
-		ActionResult actualResult = CubbyRunner.processAction(request,
-				response, new GuiceFilter());
+		ActionResult actualResult = CubbyRunner.processAction(servletContext,
+				request, response, new GuiceFilter());
 		CubbyAssert.assertPathEquals(Forward.class, "hello.jsp", actualResult);
 	}
 
