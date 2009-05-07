d8edf53852793839ba44dd7b3698d57e7aa752de
someda <someda@isenshi.com>
2009-05-07 16:15:17 +0000

    SessionScoped のサンプルを追加
    git-svn-id: https://www.seasar.org/svn/cubby/branches/2.0.x/cubby-apps@1683 b6ec7606-942a-4144-8728-ba06ebfb7920

--- cubby-example-gae/src/org/seasar/cubby/exmaple/gae/action/SearchAction.java
+++ cubby-example-gae/src/org/seasar/cubby/exmaple/gae/action/SearchAction.java
@@ -6,7 +6,6 @@ import org.seasar.cubby.action.ActionResult;
 import org.seasar.cubby.action.Form;
 import org.seasar.cubby.action.Forward;
 import org.seasar.cubby.action.RequestMethod;
-import org.seasar.cubby.action.RequestParameter;
 import org.seasar.cubby.exmaple.gae.dto.SearchDto;
 
 import com.google.inject.Inject;
@@ -22,7 +21,6 @@ public class SearchAction extends Action {
 	}
 
 	@Inject
-	@RequestParameter
 	public void setSearchDto(SearchDto searchDto) {
 		this.searchDto = searchDto;
 	}
