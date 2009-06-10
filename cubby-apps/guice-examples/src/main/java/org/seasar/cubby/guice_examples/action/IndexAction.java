5623310c60c9032ac03a82018b4e3169a2a59fb7
Yasuyuki Baba <yasuyuki.baba@gmail.com>
2009-06-10 14:14:41 +0000

    POJO に
    git-svn-id: https://www.seasar.org/svn/cubby/branches/2.0.x@1789 b6ec7606-942a-4144-8728-ba06ebfb7920

--- cubby-apps/guice-examples/src/main/java/org/seasar/cubby/guice_examples/action/IndexAction.java
+++ cubby-apps/guice-examples/src/main/java/org/seasar/cubby/guice_examples/action/IndexAction.java
@@ -1,6 +1,5 @@
 package org.seasar.cubby.guice_examples.action;
 
-import org.seasar.cubby.action.Action;
 import org.seasar.cubby.action.ActionResult;
 import org.seasar.cubby.action.Forward;
 import org.seasar.cubby.action.Path;
@@ -9,7 +8,7 @@ import com.google.inject.servlet.RequestScoped;
 
 @RequestScoped
 @Path("/")
-public class IndexAction extends Action {
+public class IndexAction {
 
 	public ActionResult index() {
 		return new Forward("index.jsp");
