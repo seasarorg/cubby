023c8e29588747e372dfabe62b11b218242d9567
Yasuyuki Baba <yasuyuki.baba@gmail.com>
2009-06-12 02:32:31 +0000

    スーパークラスのフィールドの検索ができていなかったのを修正
    git-svn-id: https://www.seasar.org/svn/cubby/branches/2.0.x/cubby-project@1792 b6ec7606-942a-4144-8728-ba06ebfb7920

--- cubby/src/main/java/org/seasar/cubby/internal/util/ReflectionUtils.java
+++ cubby/src/main/java/org/seasar/cubby/internal/util/ReflectionUtils.java
@@ -17,22 +17,44 @@ package org.seasar.cubby.internal.util;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.Collections;
 
+/**
+ * リフレクションのためのユーティリティクラスです。
+ * 
+ * @author baba
+ */
 public class ReflectionUtils {
 
-	public static Field[] findAllDeclaredField(Class<?> clazz) {
-		List<Field> fields = new ArrayList<Field>(50);
+	/**
+	 * 指定されたクラスとそのすべてのスーパークラスに定義されたフィールドを取得します。
+	 * 
+	 * @param clazz
+	 *            フィールドを検索するクラス
+	 * @return 検索したフィールドのコレクション
+	 */
+	public static Collection<Field> findAllDeclaredField(final Class<?> clazz) {
+		final Collection<Field> fields = new ArrayList<Field>(50);
 		appendFields(clazz, fields);
-		return fields.toArray(new Field[fields.size()]);
+		return Collections.unmodifiableCollection(fields);
 	}
 
-	private static void appendFields(Class<?> clazz, List<Field> fields) {
-		for (Field field : clazz.getDeclaredFields()) {
+	/**
+	 * 指定されたクラスとそのすべてのスーパークラスに定義されたフィールドを指定されたコレクションに追加します。
+	 * 
+	 * @param clazz
+	 *            フィールドを検索するクラス
+	 * @param fields
+	 *            フィールドを追加するコレクション
+	 */
+	private static void appendFields(final Class<?> clazz,
+			final Collection<Field> fields) {
+		for (final Field field : clazz.getDeclaredFields()) {
 			fields.add(field);
 		}
-		Class<?> superClass = clazz.getSuperclass();
-		if (Object.class.equals(superClass)) {
+		final Class<?> superClass = clazz.getSuperclass();
+		if (!Object.class.equals(superClass)) {
 			appendFields(superClass, fields);
 		}
 
