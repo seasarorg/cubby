bcd60df3cc7c2a2e229a78736a6f440ccb8c12a7
Yasuyuki Baba <yasuyuki.baba@gmail.com>
2009-07-27 13:34:38 +0000

    [CUBBY-229] プロジェクト削除
    git-svn-id: https://www.seasar.org/svn/cubby/branches/2.0.x@1886 b6ec7606-942a-4144-8728-ba06ebfb7920

--- cubby-apps/snippets/src/main/java/org/seasar/cubby/snippets/ScriptRunner.java
+++
@@ -1,7 +0,0 @@
-package org.seasar.cubby.snippets;
-
-public interface ScriptRunner {
-
-	void execute(String resourceName);
-
-}
--- cubby-apps/snippets/src/main/java/org/seasar/cubby/snippets/service/HelloService.java
+++
@@ -1,7 +0,0 @@
-package org.seasar.cubby.snippets.service;
-
-public interface HelloService {
-
-	String getMessage();
-
-}
