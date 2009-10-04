6714872309ab3274c1b3d38efbe393adc0c19577
Yasuyuki Baba <yasuyuki.baba@gmail.com>
2009-10-04 18:46:08 +0000

    サンプル更新
    git-svn-id: https://www.seasar.org/svn/cubby/trunk@2001 b6ec7606-942a-4144-8728-ba06ebfb7920

--- cubby-apps/cubby-s2-examples/src/main/java/org/seasar/cubby/examples/s2/action/IndexAction.java
+++ cubby-apps/cubby-s2-examples/src/main/java/org/seasar/cubby/examples/s2/action/IndexAction.java
@@ -13,12 +13,18 @@
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-package org.seasar.cubby.s2_examples.service;
+package org.seasar.cubby.examples.s2.action;
 
-public class HelloService {
+import org.seasar.cubby.action.ActionClass;
+import org.seasar.cubby.action.ActionResult;
+import org.seasar.cubby.action.Forward;
+import org.seasar.cubby.action.Path;
 
-	public String getMessage() {
-		return "Hello!";
+@ActionClass
+@Path("/")
+public class IndexAction {
+
+	public ActionResult index() {
+		return new Forward("index.jsp");
 	}
-
 }
